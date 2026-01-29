0e0f7c6fec48c6e053f6cc2f1dab0cbdfbcd6f89
Siddhi Potdar <146317589+siddhi0127@users.noreply.github.com>
2026-01-29 16:17:30 +0530

    Create findLCM.java

---
+++ day03-easy-problems/findLCM.java
@@ -0,0 +1,11 @@
+public class findLCM{
+  static int cal_lcm(int a,int b){
+    int g=Math.max(a,b);
+    int s=Math.min(a,b);
+    if()
+  }
+  public static void main(String args[]){
+    int a=10,b=5;
+    System.out.println("LCM of a and b is :"+cal_lcm(a,b));
+  }
+}
