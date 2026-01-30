7931a9215ec65041bbd48a34d365bba9ac5c2520
Siddhi Potdar <146317589+siddhi0127@users.noreply.github.com>
2026-01-30 16:06:50 +0530

    Add LCM calculation method in findLCM.java
    Implemented LCM calculation logic in findLCM.java.

--- day03-easy-problems/findLCM.java
+++ day03-easy-problems/findLCM.java
@@ -1,8 +1,14 @@
 public class findLCM{
+  // Lcm is the smaller number who gets divided by given 2 number 
   static int cal_lcm(int a,int b){
     int g=Math.max(a,b);
     int s=Math.min(a,b);
-    if()
+    for(int i=g;i<=a*b;i+=g){
+      if(i%s==0){
+        return i;
+      }
+    }
+    return a*b;
   }
   public static void main(String args[]){
     int a=10,b=5;
