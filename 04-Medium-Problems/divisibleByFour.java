07e26c95d66d865fbb48a560f8384bd8e8247c51
Siddhi Potdar <146317589+siddhi0127@users.noreply.github.com>
2026-02-01 23:05:07 +0530

    Create divisibleByFour.java

---
+++ 04-Medium-Problems/divisibleByFour.java
@@ -0,0 +1,12 @@
+public class divisibleByFour{
+public static void main(String args[]){
+
+long n=123456;
+if(n%4==0){
+System.out.println("divisible by 4");
+}
+else{
+System.out.println("not divisible by 4 ")
+}
+}
+}
