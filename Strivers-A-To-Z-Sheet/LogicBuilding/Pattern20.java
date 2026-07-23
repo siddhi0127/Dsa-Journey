3fd2bc37dbd531fd1fbcbd4c19f9ee39a13b3e15
Siddhi Potdar <146317589+siddhi0127@users.noreply.github.com>
2026-07-23 21:38:55 +0530

    Add Pattern20 class for star pattern printing

---
+++ Strivers-A-To-Z-Sheet/LogicBuilding/Pattern20.java
@@ -0,0 +1,42 @@
+
+class Pattern20 {
+    public static void main(String[] args) {
+        int n=5;
+        int count=8;
+        int count1=2;
+        for(int i=1 ;i<=n;i++){
+            //star
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j=1;j<=count;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            count=count-2;
+        }
+        
+        //remainin half
+        for(int i=n-1;i>0;i--){
+            //star
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j=1;j<=count1;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            count1+=2;
+            System.out.println();
+        }
+    }
+}
