9adc64ea28f37e94de816404d59711f55631753f
Siddhi Potdar <146317589+siddhi0127@users.noreply.github.com>
2026-08-14 14:58:20 +0530

    Add reverseInteger.java to reverse an integer
    Implement a program to reverse an integer.

---
+++ Strivers-A-To-Z-Sheet/Array/Easy/reverseInteger.java
@@ -0,0 +1,15 @@
+
+
+class reverseInteger {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+        int n=123;
+        int rev=0;
+        while(n>0){
+        int rem=n%10;
+        rev=rev*10+rem;
+         n=n/10;
+        }
+        System.out.println("Reverse Num : "+rev);
+    }
+}
