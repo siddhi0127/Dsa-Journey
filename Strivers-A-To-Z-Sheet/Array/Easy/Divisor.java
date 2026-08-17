6eea4dd92e0011498e5527c6da68fd255af7b928
Siddhi Potdar <146317589+siddhi0127@users.noreply.github.com>
2026-08-17 10:20:13 +0530

    Add Divisor.java to print divisors of a number

---
+++ Strivers-A-To-Z-Sheet/Array/Easy/Divisor.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int num=45;
+        for(int i=1;i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
